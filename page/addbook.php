8efb125953e1fd1799a074733b53292c4430c353
Sazzad Shopno <35499555+sazzadshopno@users.noreply.github.com>
2020-02-25 14:48:15 +0600

    files updated for update and add book

--- page/addbook.php
+++ page/addbook.php
@@ -1,41 +1,50 @@
 <?php
-    include '../include/config.php';
+include '../include/config.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <?php
-    include '../include/head.php';
+include '../include/head.php';
 ?>
 
 <body>
     <?php
-        include '../include/header.php';
+    include '../include/header.php';
     ?>
     <div class="box">
         <form action="widget/bookentry.php" method="post">
             <table align="center">
                 <?php
+                include '../include/connection.php';
+
                 if (isset($_GET['isbn'])) {
                     $isbn = $_GET['isbn'];
-                    $title = 'Software Engineering';
-                    $author = 'Sommerville';
                     echo '<tr><td>ISBN No.</td><td><input type="text" name="isbn_no" value = ' . $isbn . ' required="" autocomplete="off" disabled></td></tr>';
                     if ($_GET['available'] == 'true') {
-                        echo '<tr><td>Title</td><td><input type="text" name="title" value = "'.$title.'" required="" autocomplete="off" disabled></td></tr>';
-                        echo '<tr><td>Author</td><td><input type="text" name="author" value = "'.$author.'" required="" autocomplete="off" disabled></td></tr>';
-                        echo '<tr><td>Quantity</td><td><input type="number" name="quantity" required="" autocomplete="off" ></td></tr>';
+                        $sql = "SELECT * FROM book WHERE isbn_no = $isbn LIMIT 1;";
+                        $result = $con->query($sql);
+                        $row = $result->fetch_assoc();
+                        $title = $row['book_title'];
+                        $author = $row['book_author'];
+                        $stock = $row['stock'];
+                        echo '<tr><td>Title</td><td><input type="text" name="title" value = "' . $title . '" required="" autocomplete="off" disabled></td></tr>';
+                        echo '<tr><td>Author</td><td><input type="text" name="author" value = "' . $author . '" required="" autocomplete="off" disabled></td></tr>';
+                        echo '<tr><td>Stocked</td><td><input type="number" name="stock" value = "' . $stock . '"required="" autocomplete="off" disabled></td></tr>';
+                        echo '<tr><td>Quantity</td><td><input type="number" placeholder = "Enter Quanity.." name="quantity" required="" min="0" autocomplete="off" ></td></tr>';
+                        echo '<tr><td></td><td><input type="submit" name="update" value = "UPDATE" ></td></tr>';
                     } else {
                         echo '<tr><td>Title</td><td><input type="text" placeholder = "Enter Title.." name="title" required="" autocomplete="off"></td></tr>';
                         echo '<tr><td>Author</td><td><input type="text" placeholder = "Enter Author.." name="author" required="" autocomplete="off"></td></tr>';
-                        echo '<tr><td>Quantity</td><td><input type="number" placeholder = "Enter Quanity.." name="quantity" required="" autocomplete="off" ></td></tr>';
+                        echo '<tr><td>Quantity</td><td><input type="number" placeholder = "Enter Quanity.." name="quantity" required="" min="0" autocomplete="off" ></td></tr>';
+                        echo '<tr><td></td><td><input type="submit" name="add" value = "ADD BOOK" ></td></tr>';
                     }
-                    echo '<tr><td></td><td><input type="submit" id = "button" name="add" value = "ADD BOOK" ></td></tr>';
                 } else {
-                    echo '<tr><td><input type="text" name="isbn_no" placeholder = "ISBN No."required="" autocomplete="off"></td><td><input type="submit" name="check" value = "CHECK"></td></tr>';
+                    echo '<tr><td><input type="text" name="isbn_no" placeholder = "ISBN No."required="" autocomplete="off"></td><td><input type="submit" name="validate" value = "validate"></td></tr>';
                 }
                 ?>
             </table>
         </form>
     </div>
 </body>
+
 </html>
--- page/widget/bookentry.php
+++ page/widget/bookentry.php
@@ -1,8 +1,11 @@
 <?php
-    include_once('../../include/connection.php');
-    if(isset($_POST['check'])){
+    include('../../include/connection.php');
+    if(isset($_POST['validate'])){
         $isbn = $_POST['isbn_no'];
-        $available = $_POST['isbn_no'] == '150'? 'true' : 'false';
+        $sql = "SELECT * FROM book WHERE isbn_no = $isbn LIMIT 1;";
+        $result = $con->query($sql);
+        $available = $result->num_rows == 1? 'true' : 'false';
+        $con->close();
         header("Location: http://localhost/library/page/addbook.php?isbn=$isbn&available=$available"); 
         exit();
     }else if(isset($_POST['add'])){
