7844229972f8bc7cfecd0cfcfc941b00dd5f0f23
Sazzad Shopno <35499555+sazzadshopno@users.noreply.github.com>
2020-02-25 19:03:02 +0600

    return error message

--- page/widget/bookentry.php
+++ page/widget/bookentry.php
@@ -1,7 +1,11 @@
 <?php
     include('../../include/connection.php');
     if(isset($_POST['validate'])){
-        $isbn = $_POST['isbn_no'];
+        $isbn = trim($_POST['isbn_no']);
+        if($isbn == ""){
+            header("Location: http://localhost/library/page/addbook.php?success=false");
+            exit();
+        }
         $sql = "SELECT * FROM book WHERE isbn_no = $isbn LIMIT 1;";
         $result = $con->query($sql);
         $available = $result->num_rows == 1? 'true' : 'false';
