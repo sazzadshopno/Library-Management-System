40c570d30aef5038ea93d49610467a86bf24cde3
Sazzad Shopno <35499555+sazzadshopno@users.noreply.github.com>
2020-02-25 19:19:41 +0600

    routing fixed

--- page/widget/bookentry.php
+++ page/widget/bookentry.php
@@ -3,14 +3,14 @@
     if(isset($_POST['validate'])){
         $isbn = trim($_POST['isbn_no']);
         if($isbn == ""){
-            header("Location: http://localhost/library/page/addbook.php?success=false");
+            header("Location: ../addbook.php?success=false");
             exit();
         }
         $sql = "SELECT * FROM book WHERE isbn_no = $isbn LIMIT 1;";
         $result = $con->query($sql);
         $available = $result->num_rows == 1? 'true' : 'false';
         $con->close();
-        header("Location: http://localhost/library/page/addbook.php?isbn=$isbn&available=$available"); 
+        header("Location: ../addbook.php?isbn=$isbn&available=$available"); 
         exit();
     }else if(isset($_POST['add'])){
         echo "Added";
