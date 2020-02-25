97a5abc9f4d035d82665a0d5a66fee3f0d0070de
Sazzad Shopno <35499555+sazzadshopno@users.noreply.github.com>
2020-02-25 18:12:29 +0600

    formatting

--- include/header.php
+++ include/header.php
@@ -1,3 +1,16 @@
+<?php
+    include 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/main.css?v=<?php echo time(); ?>">
+    <title><?php print $PAGE_TITLE; ?></title>
+</head>
+<body>
 <header id="header">
     <div class="navbar">
         <a href="./index.php" id="title">LIBSYS</a>
