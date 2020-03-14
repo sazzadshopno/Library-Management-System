689052096e46866255d707994052ed6a33e49730
Sazzad Shopno <Sazzadshopno6@gmail.com>
2020-03-14 21:31:20 +0600

    active issued book on going

--- page/index.php
+++ page/index.php
@@ -1,6 +1,18 @@
 <?php
-    include '../include/header.php';
+include '../include/header.php';
 ?>
-
+<div class="result">
+    <h3>Active Issued Book</h3>
+    <table align='center'>
+        <tr>
+            <th>ISBN No.</th>
+            <th>Student ID</th>
+            <th>Issue Date</th>
+            <th>Due Date</th>
+            <th>Option</th>
+        </tr>
+    </table>
+</div>
 </body>
+
 </html>
